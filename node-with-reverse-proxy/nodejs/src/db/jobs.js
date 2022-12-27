cf33577a0eefadefd9a1add00b219d08af03ef0a
Andre Postiga <andre.postiga@msn.com>
2022-12-26 22:58:57 -0300

    savechanges

---
+++ node-with-reverse-proxy/nodejs/src/db/jobs.js
@@ -0,0 +1,36 @@
+const { queryPromise } = require('./queryPromisse')
+
+async function dropTableIfExists() {
+    const sqlTable = `DROP TABLE IF EXISTS people`;
+    await queryPromise.query(sqlTable)
+}
+
+async function createTable() {
+    const sqlTable = `CREATE TABLE IF NOT EXISTS people(id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id))`;
+    await queryPromise.query(sqlTable)
+}
+
+async function addPeople() {
+    const characters = [
+        ['Fulano'], 
+        ['Beltrano'], 
+        ['Ciclano'],
+    ]
+    const sqlInsert = `INSERT INTO people(name) VALUES ?`;
+    await queryPromise.queryMultiple(sqlInsert, characters)
+}
+
+async function getAllPeople() {
+    const sqlTable = `SELECT * FROM people`;
+    let result = await queryPromise.query(sqlTable)
+    return result
+}
+
+const jobs = {
+    dropTableIfExists,
+    createTable,
+    addPeople,
+    getAllPeople
+}
+  
+module.exports = { jobs };
---
+++ node-with-reverse-proxy/nodejs/src/index.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express');
+const app = express();
+const { jobs } = require('./db/jobs')
+
+async function createApp() {
+    await jobs.dropTableIfExists();
+    await jobs.createTable()
+    await jobs.addPeople()
+
+    app.set('view engine', 'ejs');
+    app.set('views', path.resolve(__dirname, 'views'));
+
+    app.get('/', async (req, res) => {
+        res.send("Hello World!");
+    });
+
+    app.get('/desafio', async (req, res) => {
+        let result = JSON.parse(JSON.stringify(await jobs.getAllPeople()))
+        res.render('home', {people:result})
+    });
+    
+    return app;
+}
+
+module.exports = createApp
